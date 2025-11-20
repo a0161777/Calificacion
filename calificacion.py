a77f1cb03ff338aeb1cfff5fc2dfa29fbe5c9728
a0161777 <a01613777@tec.mx>
2025-11-20 09:28:26 -0600

    Update calificacion.py

--- calificacion.py
+++ calificacion.py
@@ -31,7 +31,7 @@ datos =  pd.read_csv('student_exam_scores_prediction.csv', encoding='latin-1')
 X = datos.drop(columns='exam_score')
 y = datos['exam_score']
 
-from sklearn.model_selection import train_test_split
+
 from sklearn.linear_model import LinearRegression
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=1613777)
 LR = LinearRegression()
